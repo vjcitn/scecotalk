bb28437c1c564734fcdd722195b5064b7028ea0f
vjcitn <stvjc@channing.harvard.edu>
2024-05-29 10:20:21 -0400

    add func

---
+++ process_bib.R
@@ -0,0 +1,56 @@
+# for use in a talk, no bioc source status
+wrap_pmid = function(x)
+ sprintf("<a href='https://pubmed.ncbi.nlm.nih.gov/%s'>%s</a>", x, x)
+
+
+unbrack = function (x) 
+gsub("\\{|\\}", "", x)
+
+#' transform a bibtex file to data.frame
+#' @import bibtex
+#' @import dplyr
+#' @param bibfile character(1) path to bibtex
+#' @param has.pmids logical(1) if TRUE, href tags will be generated using the pmid attribute
+#' of entries
+#' @return data.frame with rownames given by the bibtex tags
+#' @examples
+#' bibref = system.file("scripts", "ref.bib", package="scRNAseq")
+#' dem = bib_to_data.frame(bibref)
+#' head(dem)
+#' @export
+bib_to_data.frame = function(bibfile, has.pmids=TRUE) {
+ bibdat = bibtex::read.bib(bibfile)
+ titles = sapply(bibdat, function(x) unbrack(x$title))
+ if (has.pmids) {
+   pmids = sapply(bibdat, function(x)
+      wrap_pmid(x$pmid))
+   return(data.frame(title=titles, pmid=pmids))
+ }
+ data.frame(title=titles)
+}
+
+#datf = bib_to_data.frame("ref.bib")
+
+#' produce enriched data frame with pmid hrefs for studies in scRNAseq
+#' for local use only for a talk
+enhanceListDatasets = function() {
+#  bibref = system.file("scripts", "ref.bib", package="scRNAseq")
+  stopifnot(file.exists("./ref.bib"))
+  bibref = "./ref.bib"
+  bibdf = bib_to_data.frame(bibref)
+  bibdf$tag = rownames(bibdf)
+  ndf = as.data.frame(scRNAseq::listDatasets())
+  ndf$tag = sub("@", "", ndf$Reference)
+  nndf = dplyr::left_join(ndf, bibdf, by="tag")
+  taxmap = c("8355"="Xenopus laevis", "9606"="Homo sapiens", "10090"="Mus musculus")
+  data.frame(org=taxmap[as.character(nndf$Taxonomy)],
+     part = nndf$Part, title=nndf$title, count=nndf$Number, pmid=nndf$pmid, call=nndf$Call)
+}
+
+#' use DT::datatable to present information in listDatasets() with linked HTML for pubmed entries
+#' @import DT
+#' @export
+datasetsPage = function() {
+  newdf = enhanceListDatasets()
+  DT::datatable(newdf, escape=FALSE)
+}
